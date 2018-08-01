7de880099cd4fc0f5b3e3b0c104a55827c284836
Trac Nguyen <trac.nguyen@news.com.au>
2018-08-01 16:48:51 +0700

    Add animate.css loader

--- src/Wp.php
+++ src/Wp.php
@@ -15,7 +15,7 @@ class Wp {
 	 * @param bool $use_cdn
 	 */
 	public static function load_font_awesome( $use_cdn = false ) {
-		wp_enqueue_style( 'font-awesome', $use_cdn ? 'https://use.fontawesome.com/releases/v5.2.0/css/all.css' : ENPII_BASE_PLUGIN_ASSETS_URL . '/font-awesome/web-fonts-with-css/css/fontawesome-all.min.css', array(), ENPII_BASE_PLUGIN_VER, 'all' );
+		wp_enqueue_style( 'font-awesome', $use_cdn ? '//use.fontawesome.com/releases/v5.2.0/css/all.css' : ENPII_BASE_PLUGIN_ASSETS_URL . '/font-awesome/web-fonts-with-css/css/fontawesome-all.min.css', array(), ENPII_BASE_PLUGIN_VER, 'all' );
 	}
 
 	/**
@@ -23,9 +23,9 @@ class Wp {
 	 * @param bool $use_cdn
 	 */
 	public static function load_bxslider( $use_cdn = false ) {
-		wp_enqueue_style( 'bxslider', $use_cdn ? 'https://cdnjs.cloudflare.com/ajax/libs/bxslider/4.2.15/jquery.bxslider.min.css' : ENPII_BASE_PLUGIN_ASSETS_URL . '/bxslider-4/dist/jquery.bxslider.min.css', array(), ENPII_BASE_PLUGIN_VER, 'screen' );
+		wp_enqueue_style( 'bxslider', $use_cdn ? '//cdnjs.cloudflare.com/ajax/libs/bxslider/4.2.15/jquery.bxslider.min.css' : ENPII_BASE_PLUGIN_ASSETS_URL . '/bxslider-4/dist/jquery.bxslider.min.css', array(), ENPII_BASE_PLUGIN_VER, 'screen' );
 
-		wp_enqueue_script( 'bxslider', $use_cdn ? 'https://cdnjs.cloudflare.com/ajax/libs/bxslider/4.2.15/jquery.bxslider.min.js' : ENPII_BASE_PLUGIN_ASSETS_URL . '/bxslider-4/dist/jquery.bxslider.min.js', ['jquery'], ENPII_BASE_PLUGIN_VER, true );
+		wp_enqueue_script( 'bxslider', $use_cdn ? '//cdnjs.cloudflare.com/ajax/libs/bxslider/4.2.15/jquery.bxslider.min.js' : ENPII_BASE_PLUGIN_ASSETS_URL . '/bxslider-4/dist/jquery.bxslider.min.js', ['jquery'], ENPII_BASE_PLUGIN_VER, true );
 	}
 
 	/**
@@ -34,7 +34,7 @@ class Wp {
 	 */
 	public static function load_modernizr($use_cdn = false)
 	{
-		wp_enqueue_script('modernizr', 'https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js', [], ENPII_BASE_PLUGIN_VER, true);
+		wp_enqueue_script('modernizr', '//cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js', [], ENPII_BASE_PLUGIN_VER, true);
 	}
 
 	/**
@@ -44,7 +44,7 @@ class Wp {
 	public static function load_detectizr($use_cdn = false)
 	{
 		static::load_modernizr($use_cdn);
-		wp_enqueue_script('detectizr', 'https://cdnjs.cloudflare.com/ajax/libs/detectizr/2.2.0/detectizr.min.js', ['modernizr'], ENPII_BASE_PLUGIN_VER, true);
+		wp_enqueue_script('detectizr', '//cdnjs.cloudflare.com/ajax/libs/detectizr/2.2.0/detectizr.min.js', ['modernizr'], ENPII_BASE_PLUGIN_VER, true);
 	}
 
 	/**
@@ -52,6 +52,11 @@ class Wp {
 	 * @param bool $use_cdn
 	 */
 	public static function load_colorbox( $use_cdn = false ) {
-		wp_enqueue_script( 'colorbox', $use_cdn ? 'https://cdnjs.cloudflare.com/ajax/libs/jquery.colorbox/1.6.4/jquery.colorbox-min.js' : ENPII_BASE_PLUGIN_ASSETS_URL . '/jquery-colorbox/jquery.colorbox-min.js', ['jquery'], ENPII_BASE_PLUGIN_VER, true );
+		wp_enqueue_script( 'colorbox', $use_cdn ? '//cdnjs.cloudflare.com/ajax/libs/jquery.colorbox/1.6.4/jquery.colorbox-min.js' : ENPII_BASE_PLUGIN_ASSETS_URL . '/jquery-colorbox/jquery.colorbox-min.js', ['jquery'], ENPII_BASE_PLUGIN_VER, true );
+	}
+
+	public static function load_animate_css($use_cdn = false)
+	{
+		wp_enqueue_style( 'animate-css', $use_cdn ? '//cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.min.css' : '//cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.min.css', array(), ENPII_BASE_PLUGIN_VER, 'screen' );
 	}
 }
