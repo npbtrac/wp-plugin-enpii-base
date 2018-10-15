863b74fc3fb8fd838f1eec65189174202609112e
Trac Nguyen <trac.nguyen@news.com.au>
2018-10-15 15:10:04 +0700

    Change the text domain string and remove usage of vendor in plugin

--- enpii-base.php
+++ enpii-base.php
@@ -16,7 +16,6 @@ defined( 'ENPII_BASE_PLUGIN_URL' ) || define( 'ENPII_BASE_PLUGIN_URL', plugins_u
 defined( 'ENPII_BASE_PLUGIN_PATH' ) || define( 'ENPII_BASE_PLUGIN_PATH', __DIR__ );
 defined( 'ENPII_BASE_PLUGIN_ASSETS_URL' ) || define( 'ENPII_BASE_PLUGIN_ASSETS_URL', plugins_url( ENPII_BASE_PLUGIN_FOLDER_NAME ) . DIRECTORY_SEPARATOR . 'assets' );
 
-! file_exists(__DIR__ . "/vendor/autoload.php") || require_once (__DIR__ . "/vendor/autoload.php");
 
 class EnpiiBase {
 	public static $text_domain = 'enpii';
@@ -28,7 +27,7 @@ class EnpiiBase {
 		$plugin = 'advanced-custom-fields-pro/acf.php';
 		if ( ! is_plugin_active( $plugin ) ) {
 			deactivate_plugins( plugin_basename( __FILE__ ) );
-			die( __( 'Enpii Base plugin requires ACF pro', _NP_TEXT_DOMAIN ) );
+			die( __( 'Enpii Base plugin requires ACF pro', static::$text_domain ) );
 		}
 	}
 }
