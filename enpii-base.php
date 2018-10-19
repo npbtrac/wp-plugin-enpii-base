2ed1e7eb3da7a60d97f945cdfd1e2b0c77da0d0d
Trac Nguyen <trac.nguyen@news.com.au>
2018-10-19 14:02:41 +0700

    Add load config

--- enpii-base.php
+++ enpii-base.php
@@ -10,12 +10,14 @@
  * Domain Path: /languages/
  */
 
+use Enpii\Wp\EnpiiBase\Wp as Wp;
+use Enpii\Wp\EnpiiBase\Base\WebApp as WpApp;
+
 defined( 'ENPII_BASE_PLUGIN_VER' ) || define( 'ENPII_BASE_PLUGIN_VER', 0.2 );
 defined( 'ENPII_BASE_PLUGIN_FOLDER_NAME' ) || define( 'ENPII_BASE_PLUGIN_FOLDER_NAME', 'enpii-base' );
 defined( 'ENPII_BASE_PLUGIN_URL' ) || define( 'ENPII_BASE_PLUGIN_URL', plugins_url( ENPII_BASE_PLUGIN_FOLDER_NAME ) );
 defined( 'ENPII_BASE_PLUGIN_PATH' ) || define( 'ENPII_BASE_PLUGIN_PATH', __DIR__ );
 defined( 'ENPII_BASE_PLUGIN_ASSETS_URL' ) || define( 'ENPII_BASE_PLUGIN_ASSETS_URL', plugins_url( ENPII_BASE_PLUGIN_FOLDER_NAME ) . DIRECTORY_SEPARATOR . 'assets' );
-
 
 class EnpiiBase {
 	public static $text_domain = 'enpii';
@@ -23,9 +25,33 @@ class EnpiiBase {
 	static function activate() {
 		// do not generate any output
     }
+
+	static function get_default_config() {
+		$config       = [
+			'id' => 'enpii-base',
+			'basePath' => WP_CONTENT_DIR,
+			'components' => [
+
+			],
+		];
+
+		return $config;
+	}
+
+	/**
+	 * Init the Application after WordPress loaded
+	 */
+	static function init_app() {
+		Wp::load_config(static::get_default_config());
+
+		WpApp::initialize(Wp::$config);
+	}
 }
 
 register_activation_hook( __FILE__, array( 'EnpiiBase', 'activate' ) );
+
+// After WP fully loaded and before handling request
+add_action( 'wp_loaded', ['EnpiiBase', 'init_app'] );
 
 
 
