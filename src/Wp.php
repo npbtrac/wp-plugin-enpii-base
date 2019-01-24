1af58dcbf8b7f24b42da48be7819b15e5049885f
Trac Nguyen <trac.nguyen@news.com.au>
2019-01-24 13:03:10 +0700

    Repair keyword highlighting for short ones

--- src/Wp.php
+++ src/Wp.php
@@ -66,11 +66,20 @@ class Wp {
 	 * @param null|string $search_term
 	 */
 	public static function highlight_keyword( $text_to_highlight, $search_query = null, $regex_replacement = "<i class='found-text'>$0</i>", &$text_replaced_count = 0 ) {
-		$search_query        = trim( $search_query );
-		$arr_tmp             = array_unique( preg_split( '/\s+/', $search_query ) );
-		$arr_keyword_pattern = array_map( function ( $search_term ) {
-			return "/\p{L}*?" . preg_quote( $search_term ) . "\p{L}*/ui";
-		}, $arr_tmp );
+		$search_query = trim( $search_query );
+		$arr_tmp      = array_unique( preg_split( '/\s+/', $search_query ) );
+
+		$arr_keyword_pattern   = [];
+		$arr_keyword_pattern[] = "/\p{L}*?" . preg_quote( implode( ' ', $arr_tmp ) ) . "\p{L}*/i";
+		foreach ( $arr_tmp as $key => $keyword_element ) {
+			$keyword_element = str_replace( [ '"', "'" ], '', $keyword_element );
+
+			if ( strlen( $keyword_element ) > 3 ) {
+				$arr_keyword_pattern[] = "/\p{L}*" . preg_quote( $keyword_element ) . "\p{L}*/ui";
+			} else {
+				$arr_keyword_pattern[] = "/\b" . preg_quote( $keyword_element ) . "\b/ui";
+			}
+		}
 
 		return preg_replace( $arr_keyword_pattern, $regex_replacement, $text_to_highlight, - 1, $text_replaced_count );
 	}
@@ -91,10 +100,19 @@ class Wp {
 		$search_query      = trim( $search_query );
 		$text_to_highlight = preg_replace( '/[\s]+/', ' ', $text_to_highlight );
 
-		$arr_tmp             = array_unique( preg_split( '/\s+/', $search_query ) );
-		$arr_keyword_pattern = array_map( function ( $search_term ) {
-			return "/\p{L}*?" . preg_quote( $search_term ) . "\p{L}*/ui";
-		}, $arr_tmp );
+		$arr_tmp = array_unique( preg_split( '/\s+/', $search_query ) );
+
+		$arr_keyword_pattern   = [];
+		$arr_keyword_pattern[] = "/\p{L}*?" . preg_quote( implode( ' ', $arr_tmp ) ) . "\p{L}*/i";
+		foreach ( $arr_tmp as $key => $keyword_element ) {
+			$keyword_element = str_replace( [ '"', "'" ], '', $keyword_element );
+
+			if ( strlen( $keyword_element ) > 3 ) {
+				$arr_keyword_pattern[] = "/\p{L}*" . preg_quote( $keyword_element ) . "\p{L}*/ui";
+			} else {
+				$arr_keyword_pattern[] = "/\b" . preg_quote( $keyword_element ) . "\b/ui";
+			}
+		}
 
 		$arr_text_to_return = [];
 
