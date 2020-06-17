da4428909d72a582bf6eb9a7ea1112535335b294
Trac Nguyen <npbtrac@yahoo.com>
2020-06-17 19:03:40 +0700

    Repair ThemeModule

--- src/Yii2/Base/WpThemeModule.php
+++ src/Yii2/Base/WpThemeModule.php
@@ -3,9 +3,61 @@
 
 namespace Enpii\Wp\EnpiiBase\Yii2\Base;
 
+abstract class WpThemeModule extends WpModule {
 
-use yii\base\Module;
+	/** @var string the server path of the plugin */
+	public $base_path;
 
-abstract class WpThemeModule extends Module implements WpThemeInterface {
-	public $text_domain = 'default';
+	/** @var string the url to the plugin */
+	public $base_url;
+
+	/** @var string path of parent theme */
+	public $template_path;
+
+	/** @var string url of parent theme */
+	public $template_url;
+
+	/** @var string path of child theme */
+	public $stylesheet_path;
+
+	/** @var string path of child theme */
+	public $stylesheet_url;
+
+	/** @var string version of this plugin, <new_version>.<major_release>.<minor-release> */
+	public $version = '1.0.0';
+
+	/**
+	 * Execute all needed things to init the plugin
+	 *
+	 * @return void
+	 */
+	abstract protected function initTheme(): void;
+
+	/**
+	 * Initialize the plugin instance, provided that, config file is in place
+	 *
+	 * @param $base_path
+	 *
+	 * @noinspection PhpFullyQualifiedNameUsageInspection
+	 * @throws \yii\base\InvalidConfigException
+	 */
+	public static function initInstanceWithPath( $base_path ) {
+		if ( is_null( static::instance() ) ) {
+			$config = static::initConfig( $base_path );
+
+			// We use the module class name for alias and the class to initialize the module instance
+			$module_class_name = get_called_class();
+			wp_app()->setModules( [
+				$module_class_name => array_merge( [
+					'__class'         => $module_class_name,
+					'template_path'   => get_template_directory(),
+					'template_url'    => get_template_directory_uri(),
+					'stylesheet_path' => get_stylesheet_directory(),
+					'stylesheet_url'  => get_stylesheet_directory_uri(),
+				], $config )
+			] );
+		}
+		$instance = static::instance();
+		$instance->initTheme();
+	}
 }
