65adddbaf92485b2299971208d5270af690065dd
Trac Nguyen <npbtrac@yahoo.com>
2020-08-03 17:51:06 +0700

    Add base files

---
+++ src/EnpiiBase.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace Enpiicom\WpPlugin\EnpiiBase;
+
+
+use Enpiicom\WpPlugin\EnpiiBase\Libs\WpPlugin;
+
+class EnpiiBase extends WpPlugin
+{
+
+}
---
+++ src/libs/WpPlugin.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace Enpiicom\WpPlugin\EnpiiBase\Libs;
+
+
+class WpPlugin
+{
+	/**
+	 * @var string Version of this plugin
+	 */
+	public $version;
+
+	/** @noinspection PhpUnusedElementInspection */
+	/**
+	 * @var string Base path to this plugin
+	 */
+	public $basePath;
+
+	/**
+	 * @var string Base url of the folder of this plugin
+	 */
+	public $baseUrl;
+}
