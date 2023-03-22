6f12d1dc74208f4d7a9da899cc8036a9da587885
Trac Nguyen <npbtrac@yahoo.com>
2023-03-22 23:27:16 +0700

    Add WP_Theme

--- src/Libs/WP_Application.php
+++ src/Libs/WP_Application.php
@@ -7,6 +7,7 @@ namespace Enpii\WP_Plugin\Enpii_Base\Libs;
 use Enpii\WP_Plugin\Enpii_Base\Dependencies\Illuminate\Config\Repository;
 use Enpii\WP_Plugin\Enpii_Base\Dependencies\Illuminate\Foundation\Application;
 use Enpii\WP_Plugin\Enpii_Base\Libs\Interfaces\WP_Plugin_Interface;
+use Enpii\WP_Plugin\Enpii_Base\Libs\Interfaces\WP_Theme_Interface;
 use InvalidArgumentException;
 use TypeError;
 
@@ -53,6 +54,24 @@ class WP_Application extends Application {
 			]
 		);
 		$this->register( $plugin );
+	}
+
+	public function register_theme( $theme_classsname, $theme_base_path, $theme_base_url, $child_theme_base_path, $child_theme_base_url = null ): void {
+		$theme = new $theme_classsname( $this );
+		if ( ! ( $theme instanceof WP_Theme_Interface ) ) {
+			throw new InvalidArgumentException( sprintf( 'The target classname %s must implement %s', $theme_classsname, WP_Plugin_Interface::class ) );
+		}
+
+		/** @var \Enpii\WP_Plugin\Enpii_Base\Libs\WP_Theme $theme  */
+		$theme->bind_base_params(
+			[
+				WP_Theme::PARAM_KEY_THEME_BASE_PATH => $theme_base_path,
+				WP_Theme::PARAM_KEY_THEME_BASE_URL  => $theme_base_url,
+				WP_Theme::PARAM_KEY_CHILD_THEME_BASE_PATH  => $child_theme_base_path,
+				WP_Theme::PARAM_KEY_CHILD_THEME_BASE_URL  => $child_theme_base_url,
+			]
+		);
+		$this->register( $theme );
 	}
 
 	/**
