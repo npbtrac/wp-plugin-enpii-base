cf9d5e8e97509cce8f497ac51470e32af43760fa
Trac Nguyen <npbtrac@yahoo.com>
2023-12-02 20:10:48 +0700

    Adjust bootstrap file

--- enpii-base-bootstrap.php
+++ enpii-base-bootstrap.php
@@ -23,12 +23,14 @@ defined( 'ENPII_BASE_WP_API_PREFIX' ) || define(
 require_once __DIR__ . DIR_SEP . 'src' . DIR_SEP . 'helpers.php';
 
 // We include the vendor in the repo if there is no vendor loaded before
-if ( ! class_exists( \Enpii_Base\App\WP\WP_Application::class ) ) {
-	if ( version_compare( phpversion(), '8.1.0', '<' ) ) {
-		// Lower that 8.1, we load dependencies for <= 8.0, we use Laravel 7
-		require_once __DIR__ . DIR_SEP . 'vendor-laravel7' . DIR_SEP . 'autoload.php';
-	} else {
-		// PHP >= 8.1, we use Laravel 10 as the latest
-		require_once __DIR__ . DIR_SEP . 'vendor' . DIR_SEP . 'autoload.php';
-	}
+if ( version_compare( phpversion(), '8.1.0', '<' ) ) {
+	// Lower that 8.1, we load dependencies for <= 8.0, we use Laravel 7
+	$autoload_file = __DIR__ . DIR_SEP . 'vendor-laravel7' . DIR_SEP . 'autoload.php';
+} else {
+	// PHP >= 8.1, we use Laravel 10 as the latest
+	$autoload_file = __DIR__ . DIR_SEP . 'vendor' . DIR_SEP . 'autoload.php';
+}
+
+if (file_exists($autoload_file) && ! class_exists('\Enpii_Base\App\WP\WP_Application')) {
+	require_once $autoload_file;
 }
