c0c0dcc3e7fe4c99f47df59d286cb09fe628e7d0
Trac Nguyen <npbtrac@yahoo.com>
2023-12-02 20:21:24 +0700

    Adjust bootstrap file

--- src/App/Jobs/Write_Setup_Client_Script_Job.php
+++ src/App/Jobs/Write_Setup_Client_Script_Job.php
@@ -26,7 +26,7 @@ class Write_Setup_Client_Script_Job extends Base_Job {
 
 		if ( is_admin() && $current_screen->id === 'plugins' && $current_screen->parent_file === 'plugins.php' && !empty($_GET['activate']) ) {
 			$setup_url = esc_url(home_url().'/'.enpii_base_get_wp_app_prefix().''.'/wp-admin/admin/setup?force_app_running_in_console=1');
-			dev_error_log($GLOBALS['current_screen']);
+
 			echo <<<SCRIPT
 			<script type="text/javascript">
 				var enpii_base_setup_url = '$setup_url';
--- src/helpers.php
+++ src/helpers.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 use Illuminate\Filesystem\Filesystem;
 
-if ( ! function_exists( 'enpii_base_get_laravel_version' ) ) {
+if ( ! function_exists( 'enpii_base_get_major_version' ) ) {
 	function enpii_base_get_major_version( $version ): int {
 		$parts = explode( '.', $version );
 		return (int) filter_var( $parts[0], FILTER_SANITIZE_NUMBER_INT );
